f58c509c5210a696d2a72406e9e45b4b7e7b06ae
Timothy Putzke <tsputzke@gmail.com>
2019-11-20 11:43:40 -0500

    Add landing styles

--- src/Landing.js
+++ src/Landing.js
@@ -9,7 +9,7 @@ class Landing extends Component {
           <h2>Video Curation for Kids</h2>
         </section>
         <section id='hero'>
-        <h3>Hero text here. Parents can curate a list of videos that are appropriate for their child, the app then allows a child to choose videos from this list in a child friendly UI.</h3>
+        <p>Hero text here. Parents can curate a list of videos that are appropriate for their child, the app then allows a child to choose videos from this list in a child friendly UI.</p>
         </section>
         <section id='demo'>
           <h3>EMBEDDED YOUTUBE VIDEO</h3>
@@ -20,38 +20,38 @@ class Landing extends Component {
           </header>
           <form id='signin-form'>
               <div>
-                <label for="username">Username</label>
-                <input type="text" name='username' id='username' />
+                <label htmlFor="username">Username</label>
+                <input type="text" name='username' id='signin-username' />
               </div>
               <div>
-                <label for="password">Password</label>
-                <input type="password" name='password' id='password' />
+                <label htmlFor="password">Password</label>
+                <input type="password" name='password' id='signin-password' />
               </div>
               <button type='submit'>Sign In</button>
               <p>Not yet a user? Sign-up.</p>
           </form>
         </section>
-        <section id='signup'>
+        {/* <section id='signup'>
           <p>[render on 'sign-up']</p>
         <header>
           <h3>Sign-up:</h3>
         </header>
         <form id='signup-form'>
             <div>
-              <label for="username">Username</label>
-              <input type="text" name='username'id='username' />
+              <label htmlFor="username">Username</label>
+              <input type="text" name='username'id='signup-username' />
             </div>
             <div>
-              <label for="password">Password</label>
-              <input type="password" name='password'id='password' />
+              <label htmlFor="password">Password</label>
+              <input type="password" name='password'id='signup-password' />
             </div>
             <div>
-              <label for="confirm-password"> Confirm Password</label>
-              <input type="password"name='confirm-password'id='confirm-password' />
+              <label htmlFor="confirm-password"> Confirm Password</label>
+              <input type="password"name='confirm-password'id='singup-confirm-password' />
             </div>
             <button type='submit'>Sign Up</button>
         </form>
-        </section>
+        </section> */}
       </main>
     )
   }
